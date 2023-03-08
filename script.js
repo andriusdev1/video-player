e622e9d39423783a6a4e1f4e558c681810fce407
andriusdev1 <rimkunas.andrius@gmail.com>
2023-03-08 21:39:31 +0000

    Started JS trying to figure out whats wrong with the stop eventlistener

---
+++ script.js
@@ -0,0 +1,26 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+//Play and pause video
+
+
+//Update play/pause icon
+
+
+//Update progress & timestamp
+
+
+// Event listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);  //function stop(): void Cancels the document load.
+
+progress.addEventListener('change', setVideoProgress);
